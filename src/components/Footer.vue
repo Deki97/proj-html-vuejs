aea4718cb0dc82bde138234ef52baf9867f3c383
Deki97 <dejan97@libero.it>
2022-01-19 11:54:34 +0100

    Footer top done

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,17 +1,143 @@
 <template>
     <footer>
-        Ciao sono il footer
+        <div class="footer-top-wrapper">
+            <div class="my-container">
+                <div class="footer-top">
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="form">
+                                <!-- Componente FooterForm -->
+                                <FooterForm />
+                            </div>
+                        </div>
+
+                        <div class="col-4">
+                            <div class="references">
+                                <!-- Lista di menu che verrà poi presa tramite props da App.vue -->
+                                <div class="title">
+                                    <h3>Example Inc.</h3>
+                                </div>
+
+                                <div class="paragraph">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                </div>
+
+                                <div class="references-list">
+                                    <div class="single-reference">
+                                        <div class="reference-icon">
+                                            <i class="fas fa-phone-alt"></i>
+                                        </div>
+                                        
+                                        <span class="reference-text">
+                                            +1 (305) 1234-5678
+                                        </span>
+                                    </div>
+
+                                    <div class="single-reference">
+                                        <div class="reference-icon">
+                                            <i class="fas fa-envelope"></i>
+                                        </div>
+                                        
+                                        <span class="reference-text">
+                                            hello@example.com
+                                        </span>
+                                    </div>
+
+                                    <div class="single-reference">
+                                        <div class="reference-icon">
+                                            <i class="fas fa-map-marker-alt"></i>
+                                        </div>
+                                        
+                                        <span class="reference-text">
+                                            Main Avenue, 987
+                                        </span>
+                                    </div>
+                                </div>
+
+                                <div class="map-button">
+                                    <button>
+                                        View Map
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </footer>
 </template>
 
 
 <script>
+import FooterForm from './FooterForm.vue';
+
 export default {
-    name: 'Footer'
+    name: 'Footer',
+    components: {
+        FooterForm
+    }
 }
 </script>
 
 
 <style lang="scss" scoped>
+@import '../style/variables.scss';
 
+.footer-top-wrapper {
+    background-color: #111117;
+    padding: 100px 0;
+
+    .footer-top {
+        .references {
+            padding-left: 30px;
+            
+            .title {
+                h3 {
+                    color: white;
+                    font-size: 20px;
+                    margin-bottom: 20px;
+                }
+            }
+            .single-reference {
+                display: flex;
+                align-items: center;
+                margin: 20px 0;
+
+                .reference-icon {
+                    color: $page-primary-color;
+                    background-color: #0e272d;
+                    width: 35px;
+                    height: 35px;
+                    text-align: center;
+                    line-height: 35px;
+                    border-radius: 50%;
+                }
+
+                .reference-text {
+                    color: $page-primary-color;
+                    margin-left: 10px;
+                    font-size: 14px;
+                }
+            }
+
+            .references-list {
+                margin-top: 30px;
+            }
+
+            .map-button {
+                button {
+                    background-color: transparent;
+                    color: white;
+                    border: 1px solid $page-primary-color;
+                    text-transform: uppercase;
+                    font-size: 14px;
+                    padding: 5px 20px;
+                    border-radius: 3px;
+                }
+            }
+        }
+    }
+}
 </style>
---
+++ src/components/FooterForm.vue
@@ -0,0 +1,105 @@
+<template>
+    <div class="footer-form">
+        <div class="mini-title">
+            Send a Message
+        </div>
+
+        <div class="heading-title">
+            Get in 
+            <span class="contrast-green-dark">
+                Touch
+            </span>
+        </div>
+
+        <div class="paragraph">
+            <p>We will respond to your message as soon as possible.</p>
+        </div>
+
+        <form>
+            <div class="name-email d-flex justify-content-between">
+                <input type="text" placeholder="Name">
+                <input type="email" placeholder="Email">
+            </div>
+
+            <div class="phone-info d-flex justify-content-between">
+                <input type="text" placeholder="Phone">
+                
+                <select name="more-info">
+                    <option value="more-info">More Info</option>
+                </select>
+            </div>
+
+            <div class="message">
+                <textarea name="message" cols="30" rows="5" placeholder="Message"></textarea>
+            </div>
+
+            <div class="send-button">
+                <button>
+                    Send
+                </button>
+            </div>
+        </form>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'FooterForm'
+}
+</script>
+
+
+<style lang="scss" scoped>
+@import '../style/variables.scss';
+
+.footer-form {
+    .heading-title {
+        color: white;
+    }
+
+    form {
+        margin-top: 40px;
+
+        div > input,
+        select,
+        textarea {
+            color: #9a9ba2;
+            background-color: #1d1d23;
+            font-size: 14px;
+            padding: 8px;
+            border-radius: 5px;
+            margin-bottom: 10px;
+            border: 1px solid #1d1d23;
+        }
+        .name-email input,
+        .phone-info input,
+        .phone-info select{
+            width: 50%;
+            margin: 5px;
+            
+        }
+        textarea {
+            width: calc(100% - 10px);
+            margin: 5px;
+        }
+
+        .more-info {
+            option {
+                color: #9a9ba2;
+            }
+        }
+
+        button {
+            background-color: $page-primary-color;
+            margin: 10px 5px;
+            text-transform: uppercase;
+            color: white;
+            font-size: 14px;
+            border: 1px solid $page-primary-color;
+            padding: 5px 20px;
+            border-radius: 3px;
+        }
+    }
+}
+</style>
